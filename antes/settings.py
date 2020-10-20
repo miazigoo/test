0cbeee114b966f303bdc021a85d471384b708558
miazigoo <kasper6024@gmail.com>
2020-10-20 15:13:17 +0300

    antes_shop_5

--- antes/settings.py
+++ antes/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'my.deployed.url']
 # Application definition
 
 INSTALLED_APPS = [
+    'antesDB',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'antes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'antesDB', 
+        'USER': 'antes', 
+        'PASSWORD': '26Rilisis',
+        'HOST': '127.0.0.1', 'db.example.com'
+        'PORT': '5432',
     }
 }
 
