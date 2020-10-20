8bb27c7be4b3bde04247d8c7f722250e2470a31b
miazigoo <kasper6024@gmail.com>
2020-10-20 14:20:07 +0300

    antes_shop_4

--- antes/settings.py
+++ antes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r(&^kdd8zzl-s3*1b9vhrs$ck5h02w2166%x2gxv+57k(3t)&('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'my.deployed.url']
 
